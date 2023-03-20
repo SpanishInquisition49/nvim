4c4b8efcaad0b7d178adadd0ff132b7131eeb488
Gabriele Scannagatti <71408527+SpanishInquisition49@users.noreply.github.com>
2023-03-20 20:55:30 +0100

    Fix Dap

--- after/plugin/dap.lua
+++ after/plugin/dap.lua
@@ -1,9 +1,11 @@
 local dap = require('dap')
 
+dap.defaults.fallback.terminal_win_cmd = 'tabnew'
+
 -- Toggle Breakpoint
 vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint, {})
 -- Set LogPoint
-vim.keymap.set('n', '<Leader>lp', function() 
+vim.keymap.set('n', '<Leader>dl', function() 
     dap.set_breakpoint({ log_message = vim.fn.input('Log point message: ')}); 
 end)
 -- Continue Execution
@@ -16,3 +18,4 @@ vim.keymap.set("n", "<F11>", dap.step_into, {})
 vim.keymap.set("n", "<F12>", dap.step_out, {})
 -- Status Inspection
 vim.keymap.set('n', '<Leader>dr', dap.repl.open, {})
+
